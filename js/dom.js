27139e3d0718cc5366b93d63f2de1fd076809ff5
FAHIMXGG <ahasanulhaquefahim1@gmail.com>
2023-02-09 12:18:13 +0600

    mid

--- js/dom.js
+++ js/dom.js
@@ -11,3 +11,21 @@ const listLi = document.querySelectorAll('.list li')
 for(const li  of listLi){
     console.log(li.innerText);
 }
+
+document.getElementById('head').style.textAlign = 'center';
+
+const title = document.getElementById('head')
+title.setAttribute('title', 'hoga')
+
+const sections = document.querySelectorAll('section');
+//console.log(sections);
+for(const section of sections){
+    //console.log(section);
+    section.style.border= '2px solid red';
+    section.style.marginBottom= '5px';
+    section.style.borderRadius= '15px';
+    section.style.paddingLeft= '7px';
+    section.style.backgroundColor = 'lightgray';
+}
+const main2nd = document.getElementById('main2')
+main2nd.style.backgroundColor = 'yellow';
